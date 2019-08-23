48a4e3ed52c9818bb0bf379654fc97dd9edf3584
Joseph Hunt <joseph@Josephs-MacBook-Pro.local>
2019-08-23 07:34:57 -0500

    adds passport

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
+var passport = require('passport');
 var logger = require('morgan');
 
 require('./config/database');
+require('./config/passport');
 require('dotenv').config();
 
 
@@ -27,6 +29,8 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);

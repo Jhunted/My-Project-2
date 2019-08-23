aee6916e4020f1c4d8439a47f3cf9c6f63650d81
Joseph Hunt <joseph@Josephs-MacBook-Pro.local>
2019-08-23 07:29:17 -0500

    adds session middleware

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/flightJournal', {
+mongoose.connect('mongodb://localhost:27017/concertJournal', {
     useNewUrlParser: true,
     useCreateIndex: true
 });

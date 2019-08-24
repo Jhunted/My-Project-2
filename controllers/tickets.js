2b3efe4d33b15693368f11446dda432f087f1a98
Joseph Hunt <jhunted54@gmail.com>
2019-08-24 13:35:33 -0500

    adds create ticket

---
+++ controllers/tickets.js
@@ -0,0 +1,24 @@
+const Ticket = require('../models/ticket');
+const Concert = require('../models/concert');
+
+module.exports = {
+    new: newTicket,
+    create
+}
+
+function newTicket(req, res) {
+    Concert.findById(req.params.id, function(err, concert) {
+        res.render('tickets/new', { concert });
+    });
+}   
+
+function create(req, res) {
+    Concert.findById(req.params.id, function(err, concert) {
+        Ticket.create(req.body, function(err, ticket) {
+            ticket.concerts.push(concert._id);
+            ticket.save(function(err, ticket) {
+                res.redirect(`/concerts/${concert._id}`);
+            });
+        });
+    });
+}
